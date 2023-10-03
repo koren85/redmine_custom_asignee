21331e8b57f1fb94a4f33d61ffaf952efeeb35d8
koren85 <mailbobik@gmail.com>
2023-10-03 23:42:00 +0300

    попытка сделать обработку пересекающихся групп

--- lib/redmine_custom_asignee/issue_patch.rb
+++ lib/redmine_custom_asignee/issue_patch.rb
@@ -9,65 +9,100 @@ module RedmineCustomAsignee
         unloadable
         safe_attributes 'assigned_to_id'
 
+
         # def assignable_users
         #   users = project.assignable_users.to_a
         #
-        #   # Получаем сохраненные настройки плагина
         #   plugin_settings = Setting.plugin_redmine_custom_asignee
-        #
+        #   included_user_ids = []
         #   excluded_user_ids = []
         #
-        #   # Проверяем, имеются ли настройки для статуса текущей задачи
         #   if plugin_settings && plugin_settings['group_data']
         #     plugin_settings['group_data'].each do |index, data|
-        #       if data['issue_status_id'].to_s == self.status_id.to_s
-        #         # Добавляем всех пользователей из группы, связанной со статусом текущей задачи, в список исключения
-        #         group = Group.find_by(id: data['group_id'].to_i)
-        #         excluded_user_ids += group.users.pluck(:id) if group
+        #       next unless Array(data['issue_status_id']).include?(self.status_id.to_s)
+        #
+        #       selected_group_ids = Array(data['group_id']).map(&:to_i)
+        #
+        #       if data['exclude_all_except_this'].present?
+        #         included_user_ids += User.joins(:groups).where('users_groups_users_join.group_id IN (?)', selected_group_ids).pluck('users.id')
+        #         all_other_group_ids = Group.all.ids - selected_group_ids
+        #         excluded_user_ids += User.joins(:groups).where('users_groups_users_join.group_id IN (?)', all_other_group_ids).pluck('users.id')
+        #       else
+        #         excluded_user_ids += User.joins(:groups).where('users_groups_users_join.group_id IN (?)', selected_group_ids).pluck('users.id')
         #       end
         #     end
+        #
+        #     if included_user_ids.any?
+        #       users = users & User.where(id: included_user_ids).to_a
+        #     end
+        #     if excluded_user_ids.any?
+        #       users = users - User.where(id: excluded_user_ids).to_a
+        #     end
         #   end
-        #
-        #   # Исключаем пользователей из списка назначаемых
-        #   users.reject! { |user| excluded_user_ids.include?(user.id) }
-        #
-        #   #  users << author if author && author.active? && !users.include?(author)
-        #   #   users << assigned_to if assigned_to && assigned_to.active? && !users.include?(assigned_to)
         #
         #   users.sort
         # end
 
         def assignable_users
+          # 1. Получаем всех пользователей, которые могут быть назначены на проект.
           users = project.assignable_users.to_a
 
-          plugin_settings = Setting.plugin_redmine_custom_asignee
+          # 2. Инициализация списков пользователей для включения и исключения.
+          included_user_ids = []
           excluded_user_ids = []
-          forced_include_user_ids = []
+
+          plugin_settings = Setting.plugin_redmine_custom_asignee
 
           if plugin_settings && plugin_settings['group_data']
-            plugin_settings['group_data'].each do |index, data|
-              if data['issue_status_id'].to_s == self.status_id.to_s
-                group = Group.find_by(id: data['group_id'].to_i)
+            status_settings = plugin_settings['group_data'].select { |_, data| Array(data['issue_status_id']).include?(self.status_id.to_s) }
 
-                if group
-                  if data['exclude_all_except_this'].present?
-                    all_users_except_current_group = User.joins(:groups).where.not('users_groups_users_join.group_id = ?', group.id).pluck('users.id')
-                    excluded_user_ids += all_users_except_current_group
-                    forced_include_user_ids += group.users.pluck(:id)
-                  else
-                    excluded_user_ids += group.users.pluck(:id)
-                  end
-                end
+            # Если для текущего статуса нет настроек, возвращаем всех пользователей.
+            return users if status_settings.empty?
+
+            # 3. Обрабатываем сценарий с одной записью.
+            if status_settings.count == 1
+              data = status_settings.values.first
+              group_ids = Array(data['group_id']).map(&:to_i)
+
+              if data['exclude_all_except_this'].present?
+                # 3a. Включаем пользователей этой группы и исключаем всех остальных.
+                included_user_ids += User.joins(:groups).where('users_groups_users_join.group_id IN (?)', group_ids).pluck('users.id')
+                excluded_user_ids += User.joins(:groups).where.not('users_groups_users_join.group_id IN (?)', group_ids).pluck('users.id')
+              else
+                # 3b. Исключаем пользователей этой группы.
+                excluded_user_ids += User.joins(:groups).where('users_groups_users_join.group_id IN (?)', group_ids).pluck('users.id')
               end
+
+              # 4. Обрабатываем сценарий с несколькими записями.
+            else
+              # 4a. Исключаем пользователей из групп без галки, если они не входят в группы с галкой.
+              no_tick_groups = status_settings.select { |_, data| data['exclude_all_except_this'].blank? }.values.map { |data| Array(data['group_id']).map(&:to_i) }.flatten
+              tick_groups = status_settings.select { |_, data| data['exclude_all_except_this'].present? }.values.map { |data| Array(data['group_id']).map(&:to_i) }.flatten
+
+              excluded_user_ids += User.joins(:groups).where('users_groups_users_join.group_id IN (?) AND users.id NOT IN (?)', no_tick_groups, tick_groups).pluck('users.id')
+
+              # 4b. Добавляем пользователей из групп с галкой.
+              included_user_ids += User.joins(:groups).where('users_groups_users_join.group_id IN (?)', tick_groups).pluck('users.id')
             end
           end
 
-          # Пересекаем списки, чтобы пользователи с галочкой были обязательно включены
-          final_excluded_ids = excluded_user_ids - forced_include_user_ids
+          # Если у нас есть идентификаторы пользователей для включения
+          if excluded_user_ids.any?
+            users = users - User.where(id: excluded_user_ids).to_a
+          end
+             if included_user_ids.any?
+                users = users & User.where(id: included_user_ids).to_a
+              end
 
-          users.reject! { |user| final_excluded_ids.include?(user.id) }
+          users.sort
+
+
           users.sort
         end
+
+
+
+
 
 
       end
